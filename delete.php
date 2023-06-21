7ce0f6bb2f0c1c701bcb599d37e786f88806c804
Lionel-prinsloo <2178389@talnet.nl>
2023-06-21 08:54:47 +0200

    delete

---
+++ delete.php
@@ -0,0 +1,41 @@
+<?php 
+$host = 'localhost:3307';
+$db   = 'winkel';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try 
+{
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Connected to $db";
+} 
+    catch (\PDOException $e) 
+{
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+$stmt = $pdo->query("SELECT * FROM winkel.producten");
+$result = $stmt->fetchAll();
+
+foreach ($result as $row) {
+    echo $row['product_code'] . " " .$row['product_naam'] . " " . $row['prijs_per_stuck'] . $row['omschrijving'] . "<br>";
+}
+
+
+$sql = "DELETE FROM producten WHERE product_code=2";
+$stmt= $pdo->prepare($sql);
+$stmt->execute([]);
+
+$sql = "DELETE FROM producten WHERE product_code=4";
+$stmt= $pdo->prepare($sql);
+$stmt->execute([]);
+
+
+
+?>
